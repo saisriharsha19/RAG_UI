f6368452c5d67251d4b873ac94551de7fa75e650
saisriharsha19 <sriharsha121925@gmail.com>
2025-02-27 00:24:26 -0500

    Updated Config

--- chatbot-ui/next.config.ts
+++ chatbot-ui/next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/RAG_UI', // Add this if deploying to username.github.io/RAG_UI
+  basePath: '/RAG_UI/chatbot-ui', // Add this if deploying to username.github.io/RAG_UI
   images: { unoptimized: true },
   trailingSlash: true,
+  assetPrefix: './',
   distDir: 'out', 
 };
 
